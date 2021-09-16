0bbb6a4ac0316d0fed8104798b99c5c0ef9ded89
Jonas Mohr <32023338+NastyGamer@users.noreply.github.com>
2021-09-16 17:21:04 +0200

    More features and fixes
    - Added eslint
    - Reformatted using eslint
    - Added some fancy icons
    - Added parameters
    - Changed some of the text

--- index.js
+++ index.js
@@ -1,76 +1,96 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
 const got = require("got");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const chalk = require("chalk");
-const { program } = require('commander');
+const { program } = require("commander");
 
 program
-  .option('-h, --help', "Shows help message")
-  .option('-d, --delay <ms>', "Delay in ms between checks")
-  .option('-u, --url <url>', "Url of your public steam-page, i.e. https://steamcommunity.com/id/**username**");
+	.option("-h, --help", "Shows help message")
+	.option("-d, --delay <ms>", "Delay in ms between checks")
+	.option("-u, --url <url>", "Url of your public steam-page, i.e. https://steamcommunity.com/id/**username**");
 program.parse(process.argv);
+
 const options = program.opts();
 
 if (options.help) {
-    console.log("Steam-Discord-Bridge, a tool to show your currently running game in discord when using proton.");
-    console.log("Usage: ./sdb [-h] [-d] [-u]")
-    process.exit(0)
+	console.log("Steam-Discord-Bridge, a tool to show your currently running game in discord when using proton.");
+	console.log("Usage: ./sdb [-h] [-d] [-u]");
+	process.exit(0);
 }
 if (!options.delay || !options.url) {
-    console.error("Missing parameter(s). See -h for a list of required parameters.");
-    process.exit(1)
+	console.error("Missing parameter(s). See -h for a list of required parameters.");
+	process.exit(1);
 }
 
-let client = require("discord-rich-presence")("887806370827608094");
+const client = require("discord-rich-presence")("887806370827608094");
 
 const logger = {
-    info: (msg) => {
-        console.log(`[${chalk.yellowBright("Info")}]${" ".repeat(10)}${msg}`)
-    },
-    success: (msg) => {
-        console.log(`[${chalk.greenBright("Success")}]${" ".repeat(7)}${msg}`)
-    },
-    error: (msg) => {
-        console.log(`[${chalk.redBright("Error")}]${" ".repeat(9)}${msg}`)
-    }
-}
+	info: (msg) => {
+		console.log(`[${chalk.yellowBright("Info")}]${" ".repeat(10)}${msg}`);
+	},
+	success: (msg) => {
+		console.log(`[${chalk.greenBright("Success")}]${" ".repeat(7)}${msg}`);
+	},
+	error: (msg) => {
+		console.log(`[${chalk.redBright("Error")}]${" ".repeat(9)}${msg}`);
+	}
+};
 
-let playing = undefined
+let playing = undefined;
+
+client.updatePresence({
+	details: "Laughing about Windows peasants",
+	largeImageKey: "arch",
+	smallImageKey: "tux",
+	instance: true
+});
 
 setInterval(() => {
-    got(options.url).then(response => {
-        logger.info("Fetching HTML from steam-page.")
-        const dom = new JSDOM(response.body);
-        const div = dom.window.document.querySelector(".profile_in_game_name")
-        if (div == null) {
-            if(playing == undefined) {
-                logger.info("Currently not playing anything. Doing nothing.")
-            }
-            else {
-                client.disconnect()
-                playing = undefined
-            }
-        }
-        else {
-            if(playing === div.textContent) {
-                logger.info(`Still playing ${playing}. Doing nothing.`)
-            }
-            else {
-                logger.success(`Picked up new game ${div.textContent}. Changing presence.`)
-                client = require("discord-rich-presence")("887806370827608094");
-                client.updatePresence({
-                    details: div.textContent,
-                    startTimestamp: Date.now(),
-                    instance: true
-                });
-                playing = div.textContent
-            }  
-        }
-        logger.info("Waiting...")
-    }).catch(err => {
-        logger.error(err)
-    });
-}, options.delay)
+	got(options.url).then(response => {
+		logger.info("Fetching HTML from steam-page.");
+		const dom = new JSDOM(response.body);
+		const div = dom.window.document.querySelector(".profile_in_game_name");
+		if (div == null) {
+			//Currently not playing
+			if(playing === undefined) {
+				//Hasnt played in the fetch before
+				logger.info("Currently not playing anything. Doing nothing.");
+			}
+			else {
+				//Has played in the fetch before
+				client.updatePresence({
+					details: "Laughing about Windows peasants",
+					largeImageKey: "arch",
+					smallImageKey: "tux",
+					instance: true
+				});
+				playing = undefined;
+			}
+		}
+		else {
+			//Currently playing
+			logger.success(`Picked up new game ${div.textContent}. Changing presence.`);
+			if(playing === div.textContent) {
+				//Plays the same as in the fetch before
+				logger.info(`Still playing ${playing}. Doing nothing.`);
+			}
+			else {
+				//Playing something new as in the fetch before / Started playing
+				client.updatePresence({
+					details: div.textContent,
+					startTimestamp: Date.now(),
+					largeImageKey: "controller",
+					smallImageKey: "arch",
+					instance: true
+				});
+				playing = div.textContent;
+			}  
+		}
+		logger.info("Waiting...");
+	}).catch(err => {
+		logger.error(err);
+	});
+}, options.delay);
